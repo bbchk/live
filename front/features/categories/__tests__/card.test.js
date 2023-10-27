aef2f340cd76459ee7c3b762b9c311d19a2738a0
bb <bohdan.buchok@ukma.edu.ua>
2023-10-27 12:57:43 +0300

    features/categories/comps/card test

--- front/features/categories/__tests__/card.test.js
+++ front/features/categories/__tests__/card.test.js
@@ -1,10 +1,35 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { useRouter } from "next/router";
 import Card from "../comps/card"; // Adjust the import path as needed
 
-it("renders card", () => {
-  const naming = "For dogs";
-  const image = "/photos/dog.jpeg";
-  render(<Card naming={naming} image={image} />);
-  const namingElem = screen.getByText(new RegExp(naming, "i"));
-  expect(namingElem).toBeInTheDocument();
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+    };
+  },
+}));
+
+const naming = "For dogs";
+const image = "/photos/dog.jpeg";
+
+describe("Card Component", () => {
+  beforeEach(() => {
+    render(<Card naming={naming} image={image} />);
+  });
+
+  it("renders card", () => {
+    const namingElem = screen.getByText(new RegExp(naming, "i"));
+    const imageElem = screen.getByAltText(/category/i);
+    expect(namingElem).toBeInTheDocument();
+    expect(imageElem).toBeInTheDocument();
+  });
+
+  it("checks click", () => {
+    const card = screen.getByRole("button");
+    fireEvent.click(card);
+
+    const router = useRouter();
+    expect(router.push).toHaveBeenCalledWith("/");
+  });
 });
--- front/features/categories/comps/card.js
+++ front/features/categories/comps/card.js
@@ -1,8 +1,16 @@
+import { useRouter } from "next/router";
 import s from "./card.module.scss";
 
 const Card = ({ naming, image }) => {
+  const router = useRouter();
   return (
-    <div role="button" onClick={() => {}} className={`${s.cat_card} `}>
+    <div
+      role="button"
+      onClick={() => {
+        router.push(`/`);
+      }}
+      className={`${s.cat_card} `}
+    >
       <p className={`${s.naming} `}>{naming}</p>
       <img className="card-img-bottom" src={image} alt="category" />
     </div>
