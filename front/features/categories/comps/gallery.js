46caaad819ef420f9e967e039d176dbf8be46cdd
bbuukk <bohdan.buchok@ukma.edu.ua>
2023-11-10 20:47:25 +0200

    Real categories are displayed on home page

--- front/features/categories/comps/gallery.js
+++ front/features/categories/comps/gallery.js
@@ -6,6 +6,10 @@ import { useCategoryContext } from "root/hooks/useCategoryContext";
 const Gallery = () => {
   const { categories } = useCategoryContext();
 
+  // const filteredCategories = categories.filter(
+  //   (category) => category.path === null
+  // );
+
   return (
     <div id="categories" className={`${s.gallery_container}`}>
       {/* <div className={`${s.decor_line} mb-5`}></div> */}
@@ -18,14 +22,16 @@ const Gallery = () => {
         className={` container row row-cols-sm-2 row-cols-lg-3 row-cols-xxl-4 mt-1 mx-auto g-5`}
       >
         {categories &&
-          categories.map((category) => {
-            const key = uuidv4();
-            return (
-              <div key={key} className="col d-flex justify-content-center">
-                <Card naming={category.name} image={category.image} />
-              </div>
-            );
-          })}
+          categories
+            .filter((category) => category.path === null)
+            .map((category) => {
+              const key = uuidv4();
+              return (
+                <div key={key} className="col d-flex justify-content-center">
+                  <Card naming={category.name} image={category.image} />
+                </div>
+              );
+            })}
       </div>
       <div className={`${s.decor_line} mt-5`}></div>
     </div>
