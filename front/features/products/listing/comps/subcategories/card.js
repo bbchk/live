c7210da3d167c25d7034500b076d5f106eec52f7
bbuukk <bohdan.buchok@ukma.edu.ua>
2023-12-16 08:40:44 +0200

    Subcategory card design update

--- front/features/products/listing/comps/subcategories/card.js
+++ front/features/products/listing/comps/subcategories/card.js
@@ -5,20 +5,36 @@ const SubcategoryCard = ({ category }) => {
   const { name, image } = category;
 
   return (
-    <Link
-      className={`${s.card}`}
-      href={`/products/${category.path.replaceAll(",", "/")}`}
-    >
-      <img
-        src={image}
-        width={100}
-        height={100}
-        alt="subcategory image "
-        className={`${s.image}`}
-      ></img>
-      <p className={`${s.name}`}> {name}</p>
+    <Link href={`/products/${category.path.replaceAll(",", "/")}`}>
+      <div className={`${s.card}`}>
+        <img
+          src={image}
+          width={100}
+          height={100}
+          alt="subcategory image "
+          className={`${s.image}`}
+        ></img>
+        <p className={`${s.name}`}> {name}</p>
+      </div>
     </Link>
   );
 };
 
 export default SubcategoryCard;
+
+export const Box = () => {
+  return (
+    <div className="box">
+      <div className="subcategory-card">
+        <div className="overlap-group">
+          <div className="text-wrapper">For Dogs</div>
+          <img
+            className="pembroke-welsh-corgi"
+            alt="Pembroke welsh corgi"
+            src="pembroke-welsh-corgi-1.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
