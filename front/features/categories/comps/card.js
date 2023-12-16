5cc81768bc4fd555336ce1c8201086b36034ba36
bbuukk <bohdan.buchok@ukma.edu.ua>
2023-12-16 10:49:18 +0200

    Link mixins

--- front/features/categories/comps/card.js
+++ front/features/categories/comps/card.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Card = ({ category }) => {
   return (
-    <Link href={`/products/${category.path}`} className={`${s.cat_card} `}>
+    <Link href={`/products/${category.path}`} className={`${s.cat_card} link`}>
       <p className={`${s.naming} `}>{category.name}</p>
       <img className="card-img-bottom" src={category.image} alt="category" />
     </Link>
