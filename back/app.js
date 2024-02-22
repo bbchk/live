955e57bc4a8f5a02a66626b438bb65b38b1dab13
Bohdan Buchok <bohdan.buchok@ukma.edu.ua>
2024-02-22 20:49:21 +0200

    Dependency mongoose update

--- back/app.js
+++ back/app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
+
 import { productsRoutes } from "./routes/product.js";
 import { categoryRoutes } from "./routes/category.js";
 import { userRoutes } from "./routes/user.js";
 
 const app = express();
 
-//todo set default limit
 // app.use(express.json({ limit: "300mb" }));
 
 app.use(cors());
