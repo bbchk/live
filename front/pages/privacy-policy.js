d2f5f82f332a1f6ca06ab79d2bd7688177eeb860
Bohdan Buchok <bohdan.buchok@ukma.edu.ua>
2024-02-25 15:40:09 +0200

    Privacy policy page added

---
+++ front/pages/privacy-policy.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <Head>
+        <title> Живий світ | Політика конфіденційності </title>
+        <meta
+          name="description"
+          content="Живий Світ | Політика конфіденційності"
+        />
+      </Head>
+      <div className="container mt-4"></div>
+    </>
+  );
+};
+
+export default PrivacyPolicy;
