5715c41bd8c79b94efa40f5ec5ccba8470db22a2
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-03-23 15:59:12 +0200

    Description component resizable functionality
    - If text is spilled over text container, there will be button to resize it

--- front/features/products/landing/about/comps/description.js
+++ front/features/products/landing/about/comps/description.js
@@ -1,25 +1,48 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import s from "./description.module.scss";
 
 //? todo is using these libraries safe in sake of xss(cross-site-scripting)?
 import parse from "html-react-parser";
 
 const Description = ({ product }) => {
-  const [expanded, setExpanded] = useState();
+  const [expanded, setExpanded] = useState(false);
+  const textRef = useRef();
+
+  const toggleExpanded = () => {
+    if (!expanded) {
+      textRef.current.style.maxHeight = `${textRef.current.scrollHeight}px`;
+    }
+    setExpanded(!expanded);
+  };
+
+  useEffect(() => {
+    if (!expanded) {
+      textRef.current.style.maxHeight = null;
+    }
+  }, [expanded]);
+
   return (
     <>
-      <button
-        onClick={() => {
-          setExpanded(!expanded);
-        }}
-      >
-        CLIKE ME
-      </button>
-      <div
-        id="description"
-        className={`${s.description} ${expanded ? s.expanded : ""}`}
-      >
-        <div className={`${s.text}`}>{parse(product.description)}</div>
+      <div id="description" className={`${s.description}`}>
+        <div
+          ref={textRef}
+          className={`${s.text} ${expanded ? s.expanded : s.fade_out}`}
+        >
+          {parse(product.description)}
+        </div>
+        <button onClick={toggleExpanded}>
+          {expanded ? (
+            <>
+              <p>Згонути</p>
+              <i class="bi bi-chevron-up" />
+            </>
+          ) : (
+            <>
+              <p>Читати більше</p>
+              <i class="bi bi-chevron-down" />
+            </>
+          )}
+        </button>
       </div>
     </>
   );
