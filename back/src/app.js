01832496d348553c0889d0ef89602e3a2eabbe88
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-03-23 15:19:48 +0200

    Landing Description Page - Expandable Description simple template

--- back/src/app.js
+++ back/src/app.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
 
@@ -6,6 +8,7 @@ import { categoryRoutes } from "./routes/category.js";
 import { userRoutes } from "./routes/user.js";
 
 const app = express();
+console.log(process.env.PORT);
 
 app.use(cors());
 app.use(express.json());
--- front/features/products/landing/about/comps/description.js
+++ front/features/products/landing/about/comps/description.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import s from "./description.module.scss";
 
 //? todo is using these libraries safe in sake of xss(cross-site-scripting)?
 import parse from "html-react-parser";
 
 const Description = ({ product }) => {
+  const [expanded, setExpanded] = useState();
   return (
-    <div id="description" className={`${s.description}`}>
-      {/* <div className={`${s.splitter}`}></div> */}
-      <div className={`${s.text}`}>{parse(product.description)}</div>
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setExpanded(!expanded);
+        }}
+      >
+        CLIKE ME
+      </button>
+      <div
+        id="description"
+        className={`${s.description} ${expanded ? s.expanded : ""}`}
+      >
+        <div className={`${s.text}`}>{parse(product.description)}</div>
+      </div>
+    </>
   );
 };
 
