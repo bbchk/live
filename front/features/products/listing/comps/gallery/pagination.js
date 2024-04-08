43d65ab786fc2b679cd11a96772e8c8be1fb2ef5
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-04-08 08:32:16 +0300

    Pagination component refactoring and styling

--- front/features/products/listing/comps/gallery/pagination.js
+++ front/features/products/listing/comps/gallery/pagination.js
@@ -1,74 +1,82 @@
-import { useState, useEffect } from "react";
-import Pagination from "react-bootstrap/Pagination";
-import s from "./pagination.module.scss";
-import Link from "next/link";
 import { useRouter } from "next/router";
-import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {  } from "@fortawesome/free-regular-svg-icons";
+import {
+  faAnglesLeft,
+  faAnglesRight,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
-//todo styles for disabled control buttons
+import s from "./pagination.module.scss";
+import { startLoading } from "store/modalSlice";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+
 function ProductsPagination({ numPages, activePageId }) {
+  // numPages = Number(100);
+  const dispatch = useDispatch();
   const router = useRouter();
   const { categoryPath } = router.query;
+  const getPath = (pageId) => `/products/${categoryPath}/page=${pageId}`;
+
+  //todo if pageId > numPages give 404
+  //todo if pageId == activePageId does do anything
+  const PaginationItem = ({ pageId, children }) => {
+    const isActive = pageId == activePageId;
+
+    return (
+      <Link
+        href={getPath(pageId)}
+        onClick={(event) => {
+          if (isActive) {
+            event.preventDefault();
+          } else {
+            dispatch(startLoading());
+          }
+        }}
+      >
+        <li className={`${s.item} ${isActive ? s.active : ""}`}>{children}</li>
+      </Link>
+    );
+  };
 
   return (
-    <Pagination
-      size="lg"
-      className={`${s.pagination}`}
-      // bsPrefix={"products_pagination"}
-    >
-      <div
-        className={`${s.controls} ${s.left} ${
-          activePageId == 1 ? "disabled" : ""
-        }`}
-      >
-        <Pagination.First
-          href={`/products/${categoryPath}/page=1`}
-          className={`${s.item}`}
-        />
+    <nav aria-label="Page navigation example">
+      <ul className={`${s.pagination}`}>
+        <ul className={`${s.controls} ${activePageId == 1 ? s.disabled : ""}`}>
+          <PaginationItem pageId={1}>
+            <FontAwesomeIcon icon={faAnglesLeft} />
+          </PaginationItem>
+          <PaginationItem pageId={Math.max(1, Number(activePageId) - 1)}>
+            <FontAwesomeIcon icon={faAngleLeft} />
+          </PaginationItem>
+        </ul>
 
-        <Pagination.Prev
-          href={`/products/${categoryPath}/page=${Math.max(
-            1,
-            Number(activePageId) - 1
-          )}`}
-          className={`${s.item}`}
-        />
-      </div>
-
-      <div className={`${s.pages_group}`}>
-        {Array.from({ length: numPages }, (_, i) => i + 1).map((pageId) => {
-          return (
-            <Pagination.Item
-              className={pageId == activePageId ? s.itemActive : s.item}
-              active={pageId == activePageId}
-              key={pageId}
-              href={`/products/${categoryPath}/page=${pageId}`}
-              // linkClassName="product_page_item"
-            >
-              {pageId}
-            </Pagination.Item>
-          );
-        })}
-      </div>
-
-      <div
-        className={`${s.controls} ${s.right} ${
-          activePageId == numPages ? "disabled" : ""
-        }`}
-      >
-        <Pagination.Next
-          className={`${s.item}`}
-          href={`/products/${categoryPath}/page=${Math.max(
-            1,
-            Number(activePageId) + 1
-          )}`}
-        />
-        <Pagination.Last
-          className={`${s.item}`}
-          href={`/products/${categoryPath}/page=${numPages}`}
-        />
-      </div>
-    </Pagination>
+        <ul className={`${s.pages}`}>
+          {Array.from({ length: numPages }, (_, i) => i + 1).map((pageId) => {
+            return (
+              <PaginationItem key={pageId} pageId={pageId}>
+                <p>{pageId}</p>
+              </PaginationItem>
+            );
+          })}
+        </ul>
+        <ul
+          className={`${s.controls} ${
+            activePageId == numPages ? s.disabled : ""
+          }`}
+        >
+          <PaginationItem pageId={Math.max(1, Number(activePageId) + 1)}>
+            <FontAwesomeIcon icon={faAngleRight} />
+          </PaginationItem>
+          <PaginationItem pageId={numPages}>
+            <FontAwesomeIcon icon={faAnglesRight} />
+          </PaginationItem>
+        </ul>
+      </ul>
+    </nav>
   );
 }
 
