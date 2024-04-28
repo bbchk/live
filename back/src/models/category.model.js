ec8c8be32eba362902ba3e7d00fbccd6ff361db7
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-04-28 14:23:04 +0300

    Category model update
    - Added err message required fields

--- back/src/models/category.model.js
+++ back/src/models/category.model.js
@@ -3,19 +3,21 @@ import { Schema, model } from "mongoose";
 const categorySchema = new Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, "Ім'я є обов'язовим полем"],
   },
   order: {
     type: Number,
+    required: [true, "Порядок є обов'язовим полем"],
     required: true,
   },
   path: {
     type: String,
+    required: [true, "Шлях є обов'язовим полем"],
     required: true,
   },
   imagePath: {
     type: String,
-    required: true,
+    required: [true, "Зображення є обов'язовим"],
   },
   filters: {
     type: [String],
