0a77c8b5f9e035496028b2fd78dbee878e9616c8
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-04-28 16:08:24 +0300

    ErrorHandlingMiddleware update
    - Updated the error handling middleware to return a more informative error message in development node_env.
    - Added a check to see if the error is operational or not, and return a generic error message if it's not.

--- back/src/middleware/error_handling.js
+++ back/src/middleware/error_handling.js
@@ -1,14 +1,20 @@
 import { mainLogger as ml } from "#src/utils/loggers.js"; // Adjust the path as needed
 
 export const errorHandlingMiddleware = (error, req, res, next) => {
-  console.log("🚀 ~ error:", error);
   ml.error(error.stack);
 
-  error.statusCode = error.statusCode || 500;
-  error.status = error.status || "error";
+  const resError = {
+    statusCode: error.statusCode || 500,
+    status: error.status || "error",
+    message: isOperational
+      ? error.message
+      : "Something went wrong! Please try again later.",
+  };
 
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV !== "production") {
+    resError.stack = error.stack;
+    resError.error = error;
+  }
+
+  res.status(resError.statusCode).json(resError);
 };
