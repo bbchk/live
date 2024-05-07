c5448d840fd3cad0b2888685e38f2f36a39646b4
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-05-07 16:05:22 +0300

    ref

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -38,6 +38,7 @@ import Header from "comps/layout/header/header";
 
 const Footer = lazy(() => import("comps/layout/footer/footer"));
 
+// import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
@@ -59,11 +60,11 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  //this is what the solution says:
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      require("bootstrap/dist/js/bootstrap");
-      require("@popperjs/core");
-    }
+    import("bootstrap/dist/js/bootstrap");
+    import("bootstrap/dist/js/bootstrap.bundle");
+    import("@popperjs/core");
   }, []);
 
   return (
