b501485613b1c96df88de365a6d4fe4afaee53c5
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-05-09 12:26:50 +0300

    ref

--- front/features/products/listing/comps/gallery/gallery.js
+++ front/features/products/listing/comps/gallery/gallery.js
@@ -1,25 +1,90 @@
 import ListingProductCard from "./card/listing_card";
+
 import s from "./gallery.module.scss";
+
+import {
+  AutoSizer,
+  InfiniteLoader,
+  WindowScroller,
+  Grid,
+} from "react-virtualized";
+
+const MIN_COLUMNS = 4; // Number of columns
+const MIN_COLUMN_WIDTH = 250; // Minimum width for a column
 
 const ProductGallery = ({
   activeProducts: products,
   activeCategory: category,
 }) => {
+  const cellRenderer = ({ columnIndex, rowIndex, key, style, columnCount }) => {
+    const index = rowIndex * columnCount + columnIndex;
+    const product = products[index];
+
+    if (!product) {
+      return null;
+    }
+
+    return (
+      <div key={key} style={style}>
+        <ListingProductCard
+          product={product}
+          like={() => {}}
+          isLiked={false}
+          priority={index < 10}
+        />
+      </div>
+    );
+  };
+
   return (
-    //todo make main work for accesability
-    <main className={`${s.gallery}`}>
-      {products.map((product, idx) => {
-        return (
-          <ListingProductCard
-            key={product._id}
-            product={product}
-            like={() => {}}
-            isLiked={false}
-            priority={idx < 10}
-          />
-        );
-      })}
-    </main>
+    <div className={`${s.g}`}>
+      <InfiniteLoader
+        isRowLoaded={({ index }) => !!products[index]}
+        loadMoreRows={({ startIndex, stopIndex }) => {
+          // Load more products when the user scrolls
+        }}
+        rowCount={products.length}
+      >
+        {({ onRowsRendered, registerChild }) => (
+          <WindowScroller>
+            {({ height, isScrolling, onChildScroll, scrollTop }) => (
+              <AutoSizer disableHeight>
+                {({ width }) => {
+                  let columnCount = Math.floor(width / MIN_COLUMN_WIDTH);
+                  columnCount =
+                    columnCount < MIN_COLUMNS ? MIN_COLUMNS : columnCount;
+                  return (
+                    <Grid
+                      autoHeight
+                      cellRenderer={({ columnIndex, rowIndex, key, style }) =>
+                        cellRenderer({
+                          columnIndex,
+                          rowIndex,
+                          key,
+                          style,
+                          columnCount,
+                        })
+                      }
+                      columnCount={columnCount}
+                      columnWidth={width / columnCount}
+                      height={height}
+                      isScrolling={isScrolling}
+                      onScroll={onChildScroll}
+                      overscanColumnCount={0}
+                      overscanRowCount={2}
+                      rowCount={Math.ceil(products.length / columnCount)}
+                      rowHeight={400}
+                      scrollTop={scrollTop}
+                      width={width}
+                    />
+                  );
+                }}
+              </AutoSizer>
+            )}
+          </WindowScroller>
+        )}
+      </InfiniteLoader>
+    </div>
   );
 };
 
