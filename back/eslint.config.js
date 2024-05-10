ee481a083f5f68f73081025ac287cb78b66a3fe2
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-05-10 14:56:55 +0300

    eslint config for back-end

--- back/eslint.config.js
+++ back/eslint.config.js
@@ -1,7 +1,36 @@
 import globals from 'globals'
-import pluginJs from '@eslint/js'
+import prettier from 'eslint-plugin-prettier'
+
+import js from '@eslint/js'
 
 export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+  js.configs.recommended,
+  {
+    files: ['**/*.{mjs,cjs,js}'],
+    languageOptions: {
+      globals: {
+        ...globals.node, // Node.js global variables
+        ...globals.es2017, // ES6 global variables
+      },
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...prettier.configs.recommended.rules,
+      semi: 'off',
+      'no-console': 1,
+      'no-unused-vars': 1,
+      indent: [2, 2],
+      'linebreak-style': [2, 'unix'],
+      quotes: [2, 'single'],
+      'prettier/prettier': 2,
+    },
+    plugins: {
+      prettier: prettier,
+    },
+  },
 ]
