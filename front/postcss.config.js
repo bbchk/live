bb092f7f75afce34b05f10427d71437a625a454b
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-05-10 19:59:32 +0300

    fix

--- front/postcss.config.js
+++ front/postcss.config.js
@@ -2,38 +2,38 @@
 
 module.exports = {
   plugins: [
-    'postcss-flexbugs-fixes',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
-        },
-        stage: 3,
-        features: {
-          'custom-properties': false,
-        },
-      },
-    ],
-    [
-      '@fullhuman/postcss-purgecss',
-      {
-        content: [
-          './pages/**/*.{js,jsx,ts,tsx}',
-          './comps/**/*.{js,jsx,ts,tsx}',
-          './features/**/*.{js,jsx,ts,tsx}',
-          './styles/**/*.{scss,css}',
-          './node_modules/react-bootstrap/**/*.{js,jsx,ts,tsx}',
-          './node_modules/react-bootstrap/**/*.{scss,css}',
-        ],
-        safelist: {
-          standard: ['html', 'body', 'btn'],
-          deep: [/^col/, /^navbar/, /^nav/, /^modal/],
-        },
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        whitelist: require('purgecss-with-wordpress').whitelist, // Whitelist all classes
-        whitelistPatterns: require('purgecss-with-wordpress').whitelistPatterns, // Whitelist based on patterns
-      },
-    ],
+    // 'postcss-flexbugs-fixes',
+    // [
+    //   'postcss-preset-env',
+    //   {
+    //     autoprefixer: {
+    //       flexbox: 'no-2009',
+    //     },
+    //     stage: 3,
+    //     features: {
+    //       'custom-properties': false,
+    //     },
+    //   },
+    // ],
+    // [
+    //   '@fullhuman/postcss-purgecss',
+    //   {
+    //     content: [
+    //       './pages/**/*.{js,jsx,ts,tsx}',
+    //       './comps/**/*.{js,jsx,ts,tsx}',
+    //       './features/**/*.{js,jsx,ts,tsx}',
+    //       './styles/**/*.{scss,css}',
+    //       './node_modules/react-bootstrap/**/*.{js,jsx,ts,tsx}',
+    //       './node_modules/react-bootstrap/**/*.{scss,css}',
+    //     ],
+    //     safelist: {
+    //       standard: ['html', 'body', 'btn'],
+    //       deep: [/^col/, /^navbar/, /^nav/, /^modal/],
+    //     },
+    //     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    //     // whitelist: require('purgecss-with-wordpress').whitelist, // Whitelist all classes
+    //     // whitelistPatterns: require('purgecss-with-wordpress').whitelistPatterns, // Whitelist based on patterns
+    //   },U
+    // ],
   ],
 }
