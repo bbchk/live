b97a2fb7a26ad23275cb89717055fa5b8ad233ff
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-05-10 14:32:42 +0300

    ref

--- back/src/controllers/category/create.category_controller.js
+++ back/src/controllers/category/create.category_controller.js
@@ -1,7 +1,7 @@
-import * as categoryService from '#src/services/category/create.category_service.js';
-import { asyncErrorHandler } from '#src/utils/async_error_handler.js';
+import * as categoryService from '#src/services/category/create.category_service.js'
+import { asyncErrorHandler } from '#src/utils/async_error_handler.js'
 
 export const createCategory = asyncErrorHandler(async (req, res, next) => {
-  const createdCategory = await categoryService.createCategory(req.body);
-  res.status(200).json(createdCategory);
-});
+  const createdCategory = await categoryService.createCategory(req.body)
+  res.status(200).json(createdCategory)
+})
