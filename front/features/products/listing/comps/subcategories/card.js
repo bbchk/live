5adf2c0212d2c8fe080536fda54e90e32a5139f9
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-05-15 16:20:08 +0300

    ref

--- front/features/products/listing/comps/subcategories/card.js
+++ front/features/products/listing/comps/subcategories/card.js
@@ -21,7 +21,7 @@ const SubcategoryCard = ({ category }) => {
         fallbackSrc={'/assets/goods_placeholder.svg'}
         width={150}
         height={150}
-        sizes='(max-width: 768px) 25vw,(max-width: 1200px) 151vw, 5vw'
+        sizes='(max-width: 768px) 25vw,(max-width: 1200px) 151vw, 10vw'
         alt='підкатегорія'
         priority
       />
