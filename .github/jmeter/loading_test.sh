6449116637c693641850b30bd454091990b67422
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-05-21 16:34:49 +0300

    ref

--- .github/jmeter/loading_test.sh
+++ .github/jmeter/loading_test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Perfomance testing scenarios
+# loops threads ramptime
 declare -A scenarios
 scenarios=(
-    ["LoadTesting"]="loops=100 threads=50 ramptime=1"
-    ["StressTesting"]="loops=500 threads=100 ramptime=5"
-    ["SpikeTesting"]="loops=1000 threads=1 ramptime=0"
-    ["EnduranceTesting"]="loops=100 threads=10 ramptime=60"
-    ["ScalabilityTesting"]="loops=100 threads=10 ramptime=1"
+    ["LoadTesting"]="500 100 10"
+    ["StressTesting"]="1000 200 20"
+    ["SpikeTesting"]="2000 1 0"
+    ["EnduranceTesting"]="500 50 120"
+    ["ScalabilityTesting"]="1000 100 10"
 )
 
 # Domain
@@ -21,11 +22,16 @@ IFS=',' read -ra ADDR <<<"$URL_PATHS"
 for scenario in "${!scenarios[@]}"; do
     params=${scenarios[$scenario]}
     IFS=' ' read -ra PARAMS <<<"$params"
+
+    echo PARAMS: loops: ${PARAMS[0]},threads: ${PARAMS[1]}, ramptime: ${PARAMS[2]}
     for URL_PATH in "${ADDR[@]}"; do
-        echo "Running $scenario for $URL_PATH"
+        echo "Running $scenario for '$URL_PATH' path"
+
         SLUG_PATH=$(echo $URL_PATH | tr '/' '_')
         SCENARIO_PATH=$(echo $scenario | tr ' ' '_')
+
         mkdir -p "./results/$SCENARIO_PATH/$SLUG_PATH"
-        ./apache-jmeter-5.6.3/bin/jmeter -n -t ./.github/jmeter/test_plan.jmx -Jdomain=$DOMAIN -Jpath=$URL_PATH -l "./results/$SCENARIO_PATH/$SLUG_PATH/$SLUG_PATH-results.jtl" -e -o "./results/$SCENARIO_PATH/$SLUG_PATH/res" -Jloops=${PARAMS[0]} -Jthreads=${PARAMS[1]} -Jramptime=${PARAMS[2]}
+
+        jmeter -n -t ./.github/jmeter/test_plan.jmx -Jdomain=$DOMAIN -Jpath=$URL_PATH -l "./results/$SCENARIO_PATH/$SLUG_PATH/$SLUG_PATH-results.jtl" -e -o "./results/$SCENARIO_PATH/$SLUG_PATH/res" -Jloops=${PARAMS[0]} -Jthreads=${PARAMS[1]} -Jramptime=${PARAMS[2]}
     done
 done
