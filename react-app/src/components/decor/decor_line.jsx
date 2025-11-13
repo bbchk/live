4fa0d371f2d475d6778d4cbbac7abebb1ad29dca
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-11-13 10:04:44 +0200

    feat:

--- react-app/src/components/decor/decor_line.jsx
+++
@@ -1,7 +0,0 @@
-import s from './decor_line.module.scss'
-
-const DecorLine = () => {
-  return <div className={`${s.decor_line}`} />
-}
-
-export default DecorLine
--- react-app/src/utils/stripHtmlTags.jsx
+++
@@ -1,5 +0,0 @@
-export function stripHtmlTags(str) {
-  if (str === null || str === '') return false
-  else str = str.toString()
-  return str.replace(/<[^>]*>/g, '')
-}
